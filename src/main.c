26b0026b479017d22da17de8ffeef1c08d8e2418
Nicolas Beny <nicolasbeny@Alesios-iMac.local>
2017-06-05 14:09:54 +0200

    .

--- src/main.c
+++ src/main.c
@@ -17,6 +17,79 @@ t_coor	*ft_init_coor(char **av)
 	return (c);
 }
 
+void	ft_trace_line_x(int xi,int yi,int xf,int yf)
+{
+	int	dx;
+	int	dy;
+	int	cumul;
+	int	x;
+	int	y;
+
+	x = xi;
+	y = yi;
+	dx = xf - xi;
+	dy = yf - yi;
+	mlx_pixel_put(c->mlx, c->win, x, y, 0xFFFFFF);
+	cumul = dx / 2;
+	x = xi + 1;
+	while (x++ <= xf)
+	{
+		cumul += dy;
+		if (cumul >= dx)
+		{
+			cumul -= dx;
+			y += 1;
+		}
+		mlx_pixel_put(c->mlx, c->win, x, y, 0xFFFFFF);
+	}
+}
+
+void	ligne(int xi,int yi,int xf,int yf)
+{
+	int dx,dy,i,xinc,yinc,cumul,x,y;
+	x = xi;
+	y = yi;
+	dx = xf - xi;
+	dy = yf - yi;
+	xinc = ( dx > 0 ) ? 1 : -1;
+	yinc = ( dy > 0 ) ? 1 : -1;
+	dx = abs(dx);
+	dy = abs(dy);
+	allume_pixel(x,y);
+	if ( dx > dy )
+	{
+		cumul = dx / 2;
+		i = 1;
+		while (i++ <= dx)
+		{
+			x += xinc;
+			cumul += dy;
+			if ( cumul >= dx )
+			{
+				cumul -= dx;
+				y += yinc;
+			}
+			allume_pixel(x,y);
+		}
+	}
+	else
+	{
+		cumul = dy / 2;
+		i = 1;
+		while (i++ <= dy)
+		{
+			y += yinc;
+			cumul += dx;
+			if ( cumul >= dy )
+			{
+				cumul -= dy;
+				x += xinc;
+			}
+			allume_pixel(x,y);
+		}
+	}
+}
+
 void	ft_trace_x(t_coor *c)
 {
 	int	x;
@@ -98,4 +171,55 @@ int		main(int ac, char **av)
 **screen.y = (map.x + map.y) * TILE_HEIGHT_HALF;
 **https://gamedevelopment.tutsplus.com/tutorials/creating-isometric-worlds-a-primer-for-game-developers--gamedev-6511
 **http://clintbellanger.net/articles/isometric_math/
+*/
+
+/*
+  void ligne(int xi,int yi,int xf,int yf) {
+  int dx,dy,cumul,x,y ;
+  x = xi ;
+  y = yi ;
+  dx = xf - xi ;
+  dy = yf - yi ;
+  allume_pixel(x,y) ;
+  cumul = dx / 2 ;
+  for ( x = xi+1 ; x <= xf ; x++ ) {
+  cumul += dy ;
+  if ( cumul >= dx ) {
+  cumul -= dx ;
+  y += 1 ; }
+  allume_pixel(x,y) ; } 
+  }
+*/
+
+/*
+  void ligne(int xi,int yi,int xf,int yf) {
+  int dx,dy,i,xinc,yinc,cumul,x,y ;
+  x = xi ;
+  y = yi ;
+  dx = xf - xi ;
+  dy = yf - yi ;
+  xinc = ( dx > 0 ) ? 1 : -1 ;
+  yinc = ( dy > 0 ) ? 1 : -1 ;
+  dx = abs(dx) ;
+  dy = abs(dy) ;
+  allume_pixel(x,y) ;
+  if ( dx > dy ) {
+  cumul = dx / 2 ;
+  for ( i = 1 ; i <= dx ; i++ ) {
+  x += xinc ;
+  cumul += dy ;
+  if ( cumul >= dx ) {
+  cumul -= dx ;
+  y += yinc ; }
+  allume_pixel(x,y) ; } }
+  else {
+  cumul = dy / 2 ;
+  for ( i = 1 ; i <= dy ; i++ ) {
+  y += yinc ;
+  cumul += dx ;
+  if ( cumul >= dy ) {
+  cumul -= dy ;
+  x += xinc ; }
+  allume_pixel(x,y) ; } }
+  }
 */
